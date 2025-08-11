7d7543526bd8c84958f89a26308a7ea9a6cec823
hemmer <915048+hemmer@users.noreply.github.com>
2025-08-11 19:25:25 +0100

    Fuji: Improve envelopes

--- src/Fuji.cpp
+++ src/Fuji.cpp
@@ -23,11 +23,11 @@ struct Fuji : Module {
 		LIGHTS_LEN
 	};
 
-	enum Mode {
+	enum GateMode {
 		AD,  // Attack-Decay
 		HOLD // Hold
 	};
-	enum Loop {
+	enum LoopMode {
 		ONE_SHOT, // One Shot
 		LOOP      // Loop
 	};
@@ -36,11 +36,14 @@ struct Fuji : Module {
 	Fuji() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
 		for (int i = 0; i < NUM_CHANNELS; i++) {
-			configParam(ATTACK1_PARAM + i, -3.f, 0.f, 0.f, string::f("Attack Ch. %d", i + 1), " s", 10.f);
-			configParam(DECAY1_PARAM + i, -3.f, 0.f, 0.f, string::f("Decay Ch. %d", i + 1), " s", 10.f);
+			configParam(ATTACK1_PARAM + i, -3.f, 0.f, -1.5f, string::f("Attack Ch. %d", i + 1), " s", 10.f);
+			configParam(DECAY1_PARAM + i, -3.f, 0.f, -1.5f, string::f("Decay Ch. %d", i + 1), " s", 10.f);
 			configSwitch(MODE1_PARAM + i, 0.f, 1.f, 0.f, string::f("Mode Ch. %d", i + 1), {"AD Mode", "Hold Mode"});
 			configSwitch(LOOP1_PARAM + i, 0.f, 1.f, 0.f, string::f("Loop Ch. %d", i + 1), {"One Shot", "Loop"});
-			configInput(GATE1_INPUT + i, string::f("Gate Ch. %d", i + 1));
+			auto gateInput = configInput(GATE1_INPUT + i, string::f("Gate Ch. %d", i + 1));
+			if (i > 0) {
+				gateInput->description = "Normalled to gate input for channel " + string::f("%d", i);
+			}
 			configOutput(OUT1_OUTPUT + i, string::f("Ch. %d", i + 1));
 		}
 
@@ -50,34 +53,81 @@ struct Fuji : Module {
 	dsp::ExponentialSlewLimiter attackSlew[NUM_CHANNELS];
 	dsp::ExponentialSlewLimiter decaySlew[NUM_CHANNELS];
 	dsp::SchmittTrigger gateTrigger[NUM_CHANNELS];
+	dsp::SchmittTrigger loopTrigger[NUM_CHANNELS];
 	dsp::PulseGenerator pulseGen[NUM_CHANNELS];
 	dsp::ClockDivider lightDivider;
+	bool rising[NUM_CHANNELS] = { false, false, false, false, false, false };
+	bool triggerChannels[NUM_CHANNELS] = { true, true, true, true, true, true };
 
 	void process(const ProcessArgs& args) override {
 		const bool updateLEDs = lightDivider.process();
 
+		float gateNormalVoltage = 0.f;
 		for (int i = 0; i < NUM_CHANNELS; i++) {
 
 			float attackTime = std::pow(10, params[ATTACK1_PARAM + i].getValue()); 	// range 1ms to 1s
 			float decayTime = std::pow(10, params[DECAY1_PARAM + i].getValue()); 	// range 1ms to 1s
-			const Mode mode = static_cast<Mode>(params[MODE1_PARAM + i].getValue());
-			const Loop loop = static_cast<Loop>(params[LOOP1_PARAM + i].getValue());
+			const GateMode gateMode = static_cast<GateMode>(params[MODE1_PARAM + i].getValue());
+			const LoopMode loopMode = static_cast<LoopMode>(params[LOOP1_PARAM + i].getValue());
 
-			if (gateTrigger[i].process(inputs[GATE1_INPUT + i].getVoltage())) {
-				pulseGen[i].trigger(1.2 * attackTime);
+			const float gateInput = inputs[GATE1_INPUT + i].getNormalVoltage(gateNormalVoltage);
+			const bool gateTriggered = gateTrigger[i].process(gateInput);
+			gateNormalVoltage = gateInput;
+
+			// if we're not in loop mode
+			if (loopMode == ONE_SHOT) {
+				// rising edge to gate input can trigger attack phase
+				if (gateTriggered) {
+					rising[i] = true;
+				}
+				// or suppress the disabling of the rising edge by later code if in hold mode
+				if (gateMode == HOLD && gateTrigger[i].isHigh()) {
+					rising[i] = true; // Continue rising edge if gate is held
+				}
+			}
+			else if (loopMode == LOOP) {
+				// loop mode ignores the gate input but resets in AD mode
+				if (triggerChannels[i] || gateTriggered) {
+					rising[i] = true; // Start rising edge
+					triggerChannels[i] = false; // Reset trigger state for this channel
+				}
 			}
 
 			// Gate is high, start attack phase
 			attackSlew[i].setRiseFallTau(attackTime, decayTime);
 
-			float slewSignal = pulseGen[i].process(args.sampleTime);
-			if (mode == HOLD) {
+			float slewSignal = rising[i];
+			if (gateMode == HOLD && loopMode == ONE_SHOT) {
 				slewSignal = std::max(slewSignal, gateTrigger[i].isHigh() ? 1.f : 0.f);
 			}
+			if (loopMode == LOOP) {
+				slewSignal = 2.f * slewSignal - 1.f; // Convert to bipolar signal for loop mode
+			}
 
-			float envelope = attackSlew[i].process(args.sampleTime, slewSignal);
+			attackSlew[i].process(args.sampleTime, slewSignal);
+			attackSlew[i].out = clamp(attackSlew[i].out, -0.8f, 0.8f);
+			float envelope = attackSlew[i].out;
 
-			outputs[OUT1_OUTPUT + i].setVoltage(envelope * 10.f); // Scale to 10V
+			float out = 0.f;
+			if (loopMode == LOOP) {
+				if (envelope < -0.799f) {
+					triggerChannels[i] = true; // Set trigger state for this channel
+				}
+				else if (envelope > 0.799) {
+					rising[i] = false; // End rising edge
+				}
+
+				// envelope is in the range [-0.8, 0.8], so scale to [-8, 8]
+				out = 10.f * envelope;
+			}
+			else {
+				if (envelope > 0.799) {
+					rising[i] = false; // End rising edge
+				}
+				out = clamp(10.f * envelope, 0.f, 8.f);
+			}
+
+			outputs[OUT1_OUTPUT + i].setVoltage(out);
 
 			if (updateLEDs) {
 				const float sampleTime = args.sampleTime * lightUpdateRate;
@@ -115,12 +165,12 @@ struct FujiWidget : ModuleWidget {
 			addParam(createParam<CKSSHoriz2>(mm2px(Vec(13.72, 23.155 + step_y * i)), module, Fuji::LOOP1_PARAM + i));
 		}
 
-		addChild(createLight<VostokWhiteNumberLed<1>>(mm2px(Vec(31.994, 19.259)), module, Fuji::NUM1_LIGHT + 0));
-		addChild(createLight<VostokWhiteNumberLed<2>>(mm2px(Vec(31.994, 37.665)), module, Fuji::NUM1_LIGHT + 1));
-		addChild(createLight<VostokWhiteNumberLed<3>>(mm2px(Vec(31.607, 56.040)), module, Fuji::NUM1_LIGHT + 2));
-		addChild(createLight<VostokWhiteNumberLed<4>>(mm2px(Vec(31.607, 74.537)), module, Fuji::NUM1_LIGHT + 3));
-		addChild(createLight<VostokWhiteNumberLed<5>>(mm2px(Vec(31.941, 92.853)), module, Fuji::NUM1_LIGHT + 4));
-		addChild(createLight<VostokWhiteNumberLed<6>>(mm2px(Vec(31.941, 111.296)), module, Fuji::NUM1_LIGHT + 5));
+		addChild(createLight<VostokOrangeNumberLed<1>>(mm2px(Vec(31.994, 19.259)), module, Fuji::NUM1_LIGHT + 0));
+		addChild(createLight<VostokOrangeNumberLed<2>>(mm2px(Vec(31.994, 37.665)), module, Fuji::NUM1_LIGHT + 1));
+		addChild(createLight<VostokOrangeNumberLed<3>>(mm2px(Vec(31.607, 56.040)), module, Fuji::NUM1_LIGHT + 2));
+		addChild(createLight<VostokOrangeNumberLed<4>>(mm2px(Vec(31.607, 74.537)), module, Fuji::NUM1_LIGHT + 3));
+		addChild(createLight<VostokOrangeNumberLed<5>>(mm2px(Vec(31.941, 92.853)), module, Fuji::NUM1_LIGHT + 4));
+		addChild(createLight<VostokOrangeNumberLed<6>>(mm2px(Vec(31.941, 111.296)), module, Fuji::NUM1_LIGHT + 5));
 
 
 	}
